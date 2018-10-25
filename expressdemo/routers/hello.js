ee82eafbfa8212c8f23623f5b4d185a723820232
OuYangResume <15180191339@139.com>
2018-10-25 14:30:34 +0800

    测试express提供的接口。并解决跨域问题

--- expressdemo/routers/hello.js
+++ expressdemo/routers/hello.js
@@ -6,6 +6,15 @@ const router =experss.Router()
 router.get('/',(req,res,next)=>{
     res.send("hello")
 })
+
+router.get('/userData',(req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*') //解决跨域问题
+    let userData=[
+        {name:"oouyang",age:23},
+        {name:"zhangsan",age:22}
+    ]
+    res.send(userData);
+})
 //get路由
 router.get('/:id', (req, res) => {
     console.dir(req.params); //打印对象
--- vueclidemo/src/views/index.vue
+++ vueclidemo/src/views/index.vue
@@ -1,5 +1,36 @@
 <template>
     <div>
-        zhuye
+        <div v-for="(item,index) in userList" :key="index">
+            <div>{{item.name}}</div>
+            <div>{{item.age}}</div>
+        </div>
     </div>
 </template>
+
+
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            name: "zhuye",
+            userList: []
+        };
+    },
+    mounted() {
+        this.initGetData();
+    },
+    methods: {
+        //测试express框架提供的后台服务
+        initGetData() {
+            var vm = this;
+            axios.get("http://localhost:8081/hello/userData").then(res => {
+                console.log(res);
+                vm.userList = res.data;
+            });
+        }
+    }
+};
+</script>
+
+
