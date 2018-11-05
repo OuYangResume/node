bf57c82a653c853316abe1b97cbd3f70087731bf
OuYangResume <15180191339@139.com>
2018-11-05 23:14:39 +0800

    文件上传进口

--- expressdemo/routers/user.js
+++ expressdemo/routers/user.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const connection = require('../uilts/mysqlEngine') //数据库配置
-let bodyParser = require('body-parser') //表单请求
+let bodyParser = require('body-parser') //表单请求 
 
+var fs = require('fs');
+var multer  = require('multer') //文件上传
+var upload = multer({ dest: 'uploads/' }) //设置文件上传目录
 
 var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser
@@ -35,6 +38,11 @@ router.post('/insterUser',urlencodedParser,(req,res)=>{
 router.post('/insterJsonUser',jsonParser,(req,res)=>{
     console.dir(req.body);
     res.send(req.body.name)
+})
+//文件上传
+router.post('/upload',upload.single('avatar'),(req,res)=>{
+    //avatar 是文件的上传的name
+    res.send("上传成功")
 })
 
 module.exports= router
