031db053a43291a32359625ee05fc5e232acb143
OuYangResume <15180191339@139.com>
2018-11-17 17:51:15 +0800

    ？。？

--- vueclidemo/src/views/photo/components/headPage.vue
+++ vueclidemo/src/views/photo/components/headPage.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="headpage">
         <div class="main">
-            <div>
+            <div class="title">
                 文字
             </div>
-            <div>
-                照片
+            <div class="photo">
+                <div>
+                    <img src="@/assets/images/shixia.jpg" alt="" srcset="">
+                </div>
+                <div><img src="@/assets/images/shixia.jpg" alt="" srcset=""></div>
+                <div><img src="@/assets/images/shixia.jpg" alt="" srcset=""></div>
+                <div><img src="@/assets/images/shixia.jpg" alt="" srcset=""></div>
             </div>
         </div>
     </div>
@@ -24,8 +29,36 @@ export default {};
   .main {
     display: flex;
     width: 80%;
-    height: 60%;
-    border: 1px solid
+    height: 65%;
+    border: 1px solid;
+    .title {
+      width: 30%;
+    }
+    .photo {
+      width: 70%;
+      display: flex;
+      flex-wrap: wrap;
+      div {
+        width: 50%;
+        height: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        transition: opacity 0.5s;
+        &:hover {
+          opacity: 0.6;
+        }
+        img {
+          width: 300px;
+          min-width: 303px;
+          height: 210px;
+          min-height: 210px;
+          padding: 10px;
+          background-color: rgb(255, 255, 255);
+        }
+      }
+    }
   }
 }
 </style>
