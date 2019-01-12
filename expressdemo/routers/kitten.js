ac524cdbc5db41f57877720771bdc6e5e1847309
root <812634676@qq.com>
2019-01-12 14:30:51 +0800

    mongodb

--- expressdemo/routers/kitten.js
+++ expressdemo/routers/kitten.js
@@ -1,8 +1,45 @@
-const express=require('express')
-const router=express.Router();
+const express = require('express')
+const router = express.Router();
 // const connection=require('./../uilts/mongoEngine')
 var mongoose = require('mongoose')
 // mongoose.connect('mongodb://39.108.100.163:27017/test');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-module.exports=router
+// Connection URL
+//连接数据库
+//假如数据库不存在，会自动创建一个数据库
+var url = 'mongodb://127.0.0.1:27017/test';
+
+MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+    assert.strictEqual(null, err);
+    console.dir("Connection mongodb successfully to server");
+    let data =[
+        { name: "AS" }, { a: 2 }, { a: 3 }
+    ]
+    //let data=require("./../public/location.json")
+    // insertDocuments(client, data,(result) =>{
+    //     console.log(result)
+    // });
+});
+
+/**
+ * @description: 插入数据
+ * @param {type} 
+ * @return: callback
+ */
+function insertDocuments(client, data, callback) {
+    // get ths documents collection
+    var db = client.db("test");
+    // insert some documents
+    db.collection("location").insert(data, function (err, result) {
+        assert.strictEqual(err, null);
+        // assert.strictEqual(3, result.result.n);
+        // assert.strictEqual(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+
+module.exports = router
