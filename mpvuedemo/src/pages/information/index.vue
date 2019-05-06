ff97cbc3ebad76c49eb78b947b67442340bc0cc8
root <812634676@qq.com>
2019-05-06 22:06:57 +0800

    router

---
+++ mpvuedemo/src/pages/information/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="msg">
+        消息
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style  scoped>
+.msg{
+    color: #000;
+}
+</style>
---
+++ mpvuedemo/src/pages/myHome/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="message">
+       <div>我的信息</div> 
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            name:"oouyang"
+        }
+    }
+}
+</script>
+<style scoped>
+.message {
+  color: red;
+  padding: 10px;
+  text-align: center;
+}
+</style>
