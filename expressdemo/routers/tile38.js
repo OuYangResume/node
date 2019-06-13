29034c15db0daaa7ac70cd3179c47265c6e2a37d
root <812634676@qq.com>
2019-06-13 21:10:18 +0800

    tile38

--- expressdemo/routers/tile38.js
+++ expressdemo/routers/tile38.js
@@ -1,14 +1,9 @@
 
 const express = require('express')
 const router = express.Router()
+const bodyParser=require('body-parser')
 var Tile38 = require('tile38');
 var client = new Tile38({ host: '127.0.0.1', port: 9851, debug: true });
-// save a location
-client.set('fleet', 'truck4', [33.5123, -112.2693]);
-// save a location with additional fields
-client.set('fleet', 'truck5', [33.5123, -112.2693], { value: 10, othervalue: 20 });
-
-
 
 router.get('/getFleet', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*') //解决跨域问题
@@ -24,6 +19,54 @@ router.get('/getIntersectsQuery', (req, res, next) => {
     });
 })
 
+router.use(bodyParser.urlencoded({ extended: true })); //注册body
+router.use(bodyParser.json());//数据JSON类型
+router.post('/insertFleet', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*') //解决跨域问题
+    // save a location
+    client.set('fleet', 'truck4', [33.5123, -112.2693]);
+    // save a location with additional fields
+    client.set('fleet', 'truck5', [33.5123, -112.2693], { value: 10, othervalue: 20 });
+    let geoJsonObject = {
+        'type': 'Polygon',
+        'coordinates': [[[-67.13734351262877, 45.137451890638886],
+        [-66.96466, 44.8097],
+        [-68.03252, 44.3252],
+        [-69.06, 43.98],
+        [-70.11617, 43.68405],
+        [-70.64573401557249, 43.090083319667144],
+        [-70.75102474636725, 43.08003225358635],
+        [-70.79761105007827, 43.21973948828747],
+        [-70.98176001655037, 43.36789581966826],
+        [-70.94416541205806, 43.46633942318431],
+        [-71.08482, 45.3052400000002],
+        [-70.6600225491012, 45.46022288673396],
+        [-70.30495378282376, 45.914794623389355],
+        [-70.00014034695016, 46.69317088478567],
+        [-69.23708614772835, 47.44777598732787],
+        [-68.90478084987546, 47.184794623394396],
+        [-68.23430497910454, 47.35462921812177],
+        [-67.79035274928509, 47.066248887716995],
+        [-67.79141211614706, 45.702585354182816],
+        [-67.13734351262877, 45.137451890638886]]],
+        'properties': {
+            'name': 'point marker',
+            'author': 'oouyang' ,
+            'tel':'13135667053'
+        }
+    }
+    client.set('cities', 'tempe2', geoJsonObject)
+
+    res.send({"code":200})
+})
+
+router.post('/insertCities',(req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*') //解决跨域问题
+    console.dir(req.body)
+    console.log("params",req.params)
+    console.log("req.query",req.query)
+    res.send("2ooo")
+})
 
 
 let getFleet = {
