6cc48207f6b739528007532ada9f5f6a7c42ac95
root <812634676@qq.com>
2019-08-26 16:46:57 +0800

    sb

--- learnWebGL/src/index.js
+++ learnWebGL/src/index.js
@@ -2,26 +2,39 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-24 10:42:39
- * @LastEditTime: 2019-08-24 11:02:43
+ * @LastEditTime: 2019-08-26 16:44:00
  * @LastEditors: Please set LastEditors
  */
-// import VR from "./threeFour.js"
+ import VR from "./threeFour.js"
 
-// let  opt = {
-//     container: 'panoramaConianer',//容器
-//     url: 'img/p3.png',
-//     lables: [
-//         { position: { lon: -72.00, lat: 9.00 }, logoUrl: '', text: '蓝窗户' },
-//         { position: { lon: 114.12, lat: 69.48 }, logoUrl: '', text: '一片云彩' },
-//         { position: { lon: 132.48, lat: -12.24 }, logoUrl: '', text: '大海' }
-//     ],
-//     widthSegments: 60,//水平切段数
-//     heightSegments: 40,//垂直切段数（值小粗糙速度快，值大精细速度慢）
-//     pRadius: 1000,//全景球的半径，推荐使用默认值
-//     minFocalLength: 6,//镜头最小拉近距离
-//     maxFocalLength: 100,//镜头最大拉近距离
-// }
-// new VR(opt);
+let  opt = {
+    container: 'conianer',//容器
+    imageUrl: '../img/p1.jpg',//图片路径
+    cameraPara:{
+        // fov:92,
+        // asd:"Asd",
+        // far:10000
+    },
+    lables: [
+        { position: { lon: -72.00, lat: 9.00 }, logoUrl: '', text: '蓝窗户' },
+        { position: { lon: 114.12, lat: 69.48 }, logoUrl: '', text: '一片云彩' },
+        { position: { lon: 132.48, lat: -12.24 }, logoUrl: '', text: '大海' }
+    ],
+    widthSegments: 60,//水平切段数
+    heightSegments: 40,//垂直切段数（值小粗糙速度快，值大精细速度慢）
+    pRadius: 1000,//全景球的半径，推荐使用默认值
+    minFocalLength: 6,//镜头最小拉近距离
+    maxFocalLength: 100,//镜头最大拉近距离
+}
+let sss =new VR(opt);
+function animate() {
+    sss.render();
+    requestAnimationFrame(animate)
+}
 
-import VR from "./threeIndex" 
-VR.init();
+animate()
+ 
+
+// import VR from "./threeThree"
+
+// VR.init()
--- learnWebGL/src/threeFour.js
+++ learnWebGL/src/threeFour.js
@@ -2,31 +2,103 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-24 10:15:34
- * @LastEditTime: 2019-08-24 10:51:48
+ * @LastEditTime: 2019-08-26 16:43:22
  * @LastEditors: Please set LastEditors
  */
 import * as THREE from 'three'
+const {
+    PerspectiveCamera, OrthographicCamera, Vector3, Texture, SpriteMaterial, Sprite,
+    Scene, SphereGeometry, Mesh, MeshBasicMaterial, BoxGeometry,
+    ImageUtils, WebGLRenderer, DirectionalLight, AmbientLight,
+} = THREE;
 //OrbitControls不是核心的一部分。您必须将类转换为模块并单独导入它。
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-
 class VR {
     constructor(options) {
-        
-        this.setCameraPara(this,options.cameraPara,options.cameraPosition)
+        //检查参数并挂载
+        this._initParm(options)
+        this._init();
     }
 
-    setCameraPara(that, cameraPara, cameraPosition) {
-        that.cameraPara = { "fov": 90, "aspect": that.container.innerWidth / that.container.innerHeight, "near": 0.001, "far": 1000 };
-        that.cameraPosition = { "x": 0, "y": 0, "z": 0 };
+
+    _init() {
+        console.log(this);
+        this._scene = new Scene();
+        this._renderer = new WebGLRenderer();
+        this._renderer.setSize(this._container.offsetWidth, this._container.offsetHeight);
+        //一个canvas，渲染器在其上绘制输出。
+        this._container.appendChild(this._renderer.domElement);
+        //
+        this._camera = new PerspectiveCamera(this.cameraPara.fov, this.cameraPara.aspect, this.cameraPara.near, this.cameraPara.far);
+      //  this._camera.position.set(this.cameraPosition.x,this.cameraPosition.y,this.cameraPosition.z);
+        this._camera.lookAt(new Vector3(0, 0, 0))
+
+        let mesh = this._addImg(this.imageUrl);
+        this._scene.add(mesh);
+        this._camera.position.set(20, 0, 0);
+
+        //坐标插件
+        this._scene.add(new THREE.AxisHelper(1000));
+    }
+    render() {
+        this._renderer.render(this._scene, this._camera);
+    }
+    
+
+    _addImg(url) {
+        const texture = THREE.ImageUtils.loadTexture(url);
+        const material = new MeshBasicMaterial({ map: texture });
+        const geometry = new SphereGeometry(10, 256, 256);
+        // const geometry = new THREE.SphereGeometry(50, 256, 256);
+        const mesh = new Mesh(geometry, material);
+        // 渲染球体的双面
+        material.side = THREE.DoubleSide;
+        return mesh;
+    }
+
+
+    _initParm(options) {
+        //检查
+        if (!options.container) {
+            console.error("请设置必填字段container")
+            return;
+        } else {
+            this._container = document.getElementById(options.container);
+            if (this._container == null) {
+                console.error("没有找到id为" + options.container + "元素")
+                return;
+            }
+        }
+        //检查渲染图片
+        if (!options.imageUrl) {
+            console.error("请设置必填字段imageUrl")
+            return;
+        } else {
+            this._imageUrl = options.imageUrl;
+        }
+
+        //默认相机参数
+        this.cameraPara = { "fov": 90, "aspect": this._container.offsetWidth / this._container.offsetHeight, "near": 0.1, "far": 100 };
+        this.cameraPosition = { "x": 0, "y": 0, "z": 0 }; 
+        let cameraPara = options.cameraPara, cameraPosition = options.cameraPosition
         if (cameraPara) {
             for (var property in cameraPara) {
-                that.cameraPara[property] = cameraPara[property];
+                if (!this.cameraPara[property]) {
+                    console.error(property + "字段无效,请检查")
+                } else {
+                    this.cameraPara[property] = cameraPara[property];
+                }
+
             }
         }
         if (cameraPosition) {
             for (var property in cameraPosition) {
-                that.cameraPosition[property] = cameraPosition[property];
+                if (!this.cameraPosition[property]) {
+                    console.error(property + "字段无效,请检查")
+                } else {
+                    this.cameraPosition[property] = cameraPosition[property];
+                }
             }
         }
     }
