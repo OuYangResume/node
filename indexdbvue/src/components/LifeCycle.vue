6d3d029a8de1886482a43c58dc4c7474a518bdce
root <812634676@qq.com>
2020-05-26 14:40:55 +0800

    initcycle

---
+++ indexdbvue/src/components/LifeCycle.vue
@@ -0,0 +1,54 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-05-25 14:40:47
+ * @LastEditTime: 2020-05-25 18:10:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /node/indexdbvue/src/components/LifeCycle.vue
+--> 
+<template>
+  <div>
+    {{propsName}}
+    {{dataName}}
+    {{computedName}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataName: "data---oouyang"
+    };
+  },
+  props: ["propsName"],
+  computed: {
+    computedName() {
+      return "computed---oouyang";
+    }
+  },
+  methods:{
+      testAlert(){
+          alert(1)
+      }
+  },
+  beforeCreate() {
+      
+    console.log("周期名字:" + "beforeCreate");
+    //全部获取不到
+   // this.testAlert()
+    //   console.log('data:'+this.dataName)
+    //   console.log('props:'+this.propsName)
+    //   console.log('computed:'+this.computedName)
+    //   console.log('$el:'+this.$el)
+  },
+  created() {
+    console.log("周期名字:" + "created");
+    this.testAlert()
+    console.log("data:" + this.$data);
+    console.log("props:" + this.propsName);
+    console.log("computed:" + this.computedName);
+     console.log('$el:'+this.$el)
+  }
+};
+</script>
--- indexdbvue/src/views/Insert.vue
+++ indexdbvue/src/views/Insert.vue
@@ -1,13 +1,28 @@
 <!--
  * @Author: your name
  * @Date: 2020-04-07 10:51:47
- * @LastEditTime: 2020-04-07 10:52:09
+ * @LastEditTime: 2020-05-25 14:52:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /node/indexdbvue/src/views/Insert.vue
  -->
 <template>
-    <div>
-        新建数据字典
-    </div>
+  <div>
+    <div>新建数据字典</div>
+    <life-cycle :propsName="propsName"></life-cycle>
+  </div>
 </template>
+
+<script>
+import LifeCycle from "@/components/LifeCycle.vue";
+export default {
+  data() {
+    return {
+      propsName: "props----oouynag"
+    };
+  },
+  components: {
+    LifeCycle
+  }
+};
+</script>
