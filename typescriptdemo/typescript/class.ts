85ff3187d4318e8a0450b0f33842ef2f2946cd4b
root <812634676@qq.com>
2020-10-24 22:03:56 +0800

    class 单例模式

--- typescriptdemo/typescript/class.ts
+++ typescriptdemo/typescript/class.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-23 20:49:06
- * @LastEditTime: 2020-10-23 21:55:59
+ * @LastEditTime: 2020-10-24 22:03:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /node/typescriptdemo/typescript/class.ts
@@ -14,11 +14,15 @@
  * private 只允许类的内部被调用
  * protected 允许在类的内部和继承的子类调用
  */
-class Point {
+
+//抽象类
+abstract class Point {
   name = "point";
   getName() {
     return this.name;
   }
+  //抽象方法必须再子类中实现
+  abstract getArea(): number;
 }
 
 //类的继承
@@ -36,11 +40,13 @@ class Geomentry extends Point {
   getName() {
     return super.getName() + this.name; //geomentrygeomentry
   }
+  //抽象方法的实现
+  getArea() {
+    return 123;
+  }
 }
 
-let point = new Point();
 let geo = new Geomentry();
-console.log(point.getName());
 console.log(geo.getGeoName());
 console.log(geo.getName());
 
@@ -63,3 +69,38 @@ class RoundBbox extends Bbox {
 }
 
 let bbox = new Bbox("bbox");
+
+//getter setter
+class Circle {
+  //只读
+  readonly dir: number;
+  constructor(private _name: string, dir: number) {}
+  //通过get，set在外部获取私有属性----可以处理加密保护类的私有属性
+  get name() {
+    return this._name;
+  }
+  set name(name: string) {
+    this._name = name;
+  }
+}
+let circle = new Circle("circle", 23);
+console.log(circle.name);
+console.log(circle.dir);
+//circle.dir =34
+
+//单例模式
+class Style {
+  private static instance: Style;
+  //私有属性
+  private constructor() {}
+  //静态
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Style();
+    }
+    return this.instance;
+  }
+}
+
+let style1 = Style.getInstance();
+let style2 = Style.getInstance();
